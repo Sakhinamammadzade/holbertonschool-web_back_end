8591382d3a9a36075021f071715a1e44a946d374
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-04-29 15:10:11 +0400

    concat & floor

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Basic annotations - floor"""
+import math
+
+
+def floor(n: float) -> float:
+    return math.floor(n)
