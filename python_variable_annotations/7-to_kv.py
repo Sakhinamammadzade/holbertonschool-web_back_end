21610051eec43bb17c53bff205734bd252c0d0d7
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-04-29 22:26:52 +0400

    task 2 and task 7 error fixed

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Complex types - string and int/float to tuple"""
-from typing import Union,Tuple
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[int, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Complex types - string and int/float to tuple"""
     return(k, v * v) 
