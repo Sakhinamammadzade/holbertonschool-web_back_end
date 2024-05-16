fe98d84023b5e9b7789bb5ac6b611e565fc17ac9
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-05-16 11:20:22 +0400

    i forget export default

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import ClassRoom from './0-classroom';
 
-function initializeRooms(){
+export default function initializeRooms(){
     return [
         new ClassRoom(19),
         new ClassRoom(20),
