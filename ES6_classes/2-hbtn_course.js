7de40edf6c895aa0ea6c573f2f294eb75676e6b8
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-05-16 17:57:45 +0400

    damnn

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 export default class HolbertonCourse{
   constructor(name, length, students){
     if(typeof(name) !== 'string'){
@@ -20,33 +19,29 @@ export default class HolbertonCourse{
   }
 
   set name(newName){
-    if(typeof(newName) === 'string'){
-    this._name == newName
-  }
-  else{
-    throw new Error('Name must be a string.');
-  }
+    if(typeof(newName) !== 'string'){
+      throw new Error('Name must be a string.');
+    }
+  this._name == newName
+  
 }
   get length(){
     return this._length;
   }
   set length(newLength){
     if (typeof(newLength) === 'number'){
-        this._length = newLength;
-    }
-    else{
-        throw new Error('Length must be a number.')
-    }
+      throw new Error('Length must be a number.')
+     }
+     this._length = newLength;
   }
 
   get students(){
     return this._stduents = students;
   }
   set students(newStudents) {
-    if (Array.isArray(newStudents) && newStudents.every(student => typeof student === 'string')) {
-      this._students = newStudents;
-    } else {
+    if (!Array.isArray(newStudents) && newStudents.every(student => typeof student !== 'string')) {
       throw new Error('Students must be an array of strings.');
-    }
+    } 
+    this._students = newStudents;
   }
 }
