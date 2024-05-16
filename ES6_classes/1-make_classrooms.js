4e69fec4e3054015d2f194683c11d479f0083a66
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-05-16 11:05:51 +0400

    make classroms

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import ClassRoom from './0-classroom';
+
+function initializeRooms(){
+    return [
+        new ClassRoom(19),
+        new ClassRoom(20),
+        new ClassRoom(34)
+     ];
+}
