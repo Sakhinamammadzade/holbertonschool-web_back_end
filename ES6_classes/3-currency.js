43130b3b8b592478ea402b1df9a55a4d21755dbd
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-05-19 00:30:44 +0400

    eslint fix

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,28 +1,28 @@
 export default class Currency {
-    constructor(code, name) {
-      this._code = code;
-      this._name = name;
-    }
-  
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
-    }
-  
-    get code() {
-      return this._code;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set code(newCode) {
-      if (typeof newCode !== 'string') throw TypeError('code must be a string');
-      this._code = newCode;
-    }
-  
-    set name(newName) {
-      if (typeof newName !== 'string') throw TypeError('name must be a string');
-      this._name = newName;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
   }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') throw TypeError('code must be a string');
+    this._code = newCode;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') throw TypeError('name must be a string');
+    this._name = newName;
+  }
+}
