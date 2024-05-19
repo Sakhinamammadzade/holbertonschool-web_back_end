d97e25a23f8b12cdba22cc2dc9be6e882e4576bf
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-05-19 22:22:39 +0400

    task-5 eslint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,16 @@
-/* eslint disable */
 export default class Building {
-    constructor(sqft) {
-      if (
-        this.constructor !== Building
-        && typeof this.evacuationWarningMessage !== 'function'
-      ) {
-        throw Error('Class extending Building must override evacuationWarningMessage');
-      }
-  
-      this._sqft = sqft;
+  constructor(sqft) {
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
-  
-    get sqft() {
-      return this._sqft;
-    }
+
+    this._sqft = sqft;
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
