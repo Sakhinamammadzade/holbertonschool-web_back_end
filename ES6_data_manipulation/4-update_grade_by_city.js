eb1c6d248924d372fc3107e245800c1d5eaeefdb
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-05-22 13:21:54 +0400

    task-4 eslint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,11 @@
-export default function updateStudentGradeByCity(students, city, newGrades){
+export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-    const newGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
-    return {
-    ...student,
-    grade: newGrade ? newGrade.grade: 'N/A'
-    };
+      const newGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
+      return {
+        ...student,
+        grade: newGrade ? newGrade.grade: 'N/A',
+      };
     });
 }
