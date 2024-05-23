8ba00cd593181e5bb903b3358e5af009309a2a1e
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-05-23 14:38:07 +0400

    weakmap

---
+++ ES6_data_manipulation/100-weak.js
@@ -0,0 +1,15 @@
+const weakMap = new WeakMap();
+
+const queryAPI = (endpoint) => {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  const count = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, count);
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+};
+
+export { weakMap, queryAPI };
