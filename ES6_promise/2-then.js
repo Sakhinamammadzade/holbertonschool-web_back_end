a644389f12990fb512c212fdc88e3f3da70bf06a
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-05-24 19:13:44 +0400

    catch me, if u can

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then((response) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch((error) => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
+}
