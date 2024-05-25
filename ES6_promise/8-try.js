55fb987d87e6cd8eaec5a9dba773ac1cea50b977
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-05-26 00:47:34 +0400

    throw an error

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,8 +1,4 @@
 export default function divideFunction(numerator, denominator) {
-  return new Promise((resolve, reject) => {
-    if (denominator === 0) {
-      reject('cannot divide by 0.');
-    }
-    resolve(numerator / denominator);
-  });
+  if (denominator === 0) throw new Error('cannot divide by 0');
+  return numerator / denominator;
 }
